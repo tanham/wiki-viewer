f514b511b3135dd50cecd0d4ee95a58d9b4d598b
Tanisha <tanishahampden@gmail.com>
2016-12-01 16:19:51 +0700

    add function

--- wiki-viewer.js
+++ wiki-viewer.js
@@ -1,12 +1,63 @@
+//retrieve random wikipedia page on click 
+window.onload = function randomButton() {
+	var randomPage = document.getElementById('random');
+	randomPage.addEventListener('click', randomButton, false);
 
-//store search term 
-//use search input to make call to wikipedia api 
-//return wikipedia article 
-
-var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + '&format=json&callback=?'
-
-function randomPage() {
-	//click buttom to see random wikipedia article
-	window.location='https://en.wikipedia.org/wiki/Special:Random';
+	function randomButton() {
+		window.location.assign('https://en.wikipedia.org/wiki/Special:Random');
+	}
 }
-console.log(randomPage());
+
+window.onload = function storeInput(){
+	var searchTerm = document.getElementById('input').value;
+	var searchButton = document.getElementById('search');
+	
+	searchButton.addEventListener('click', printInput, false);
+	
+	function printInput() {
+		console.log(searchTerm);
+	}
+	var customUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm + 
+		'&format=json&callback=?';
+
+	getXhr(customUrl, function(request){
+	    var response = request.target.responseText;
+	    console.log(response);
+	});
+};
+		
+	
+	
+
+function getXhr(url, success) {
+	    var xhr = new XMLHttpRequest();
+	    if (!('withCredentials' in xhr)) xhr = new XDomainRequest(); 
+	    xhr.open('GET', url);
+	    xhr.onload = success;
+	    xhr.send();
+	    return xhr;
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
